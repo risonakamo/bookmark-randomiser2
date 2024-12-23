7550ef88bf6751812bbcb5d2ade94902985b9dd9
risonakamo <hmykanae@gmail.com>
2024-12-23 14:14:58 -0500

    prototype ui layout of session create page

---
+++ doc/session create page.md
@@ -0,0 +1,12 @@
+# prototype features
+- [ ] navigate through bookmarks
+- [ ] select folders with check, it should appear in the checkout zone
+- [ ] selected folders should show number of bookmarks
+- [ ] should show total bookmarks when session created
+- [ ] path navigation should work
+
+# other ideas
+- since using a browser style, there's not much reason for the browser part to be so big. maybe try making it take up centre of screen with big side padding, and maybe even some top padding
+- fuzzy search bar
+    - look up fuzzy library
+    - when displaying results, should show the entire path somewhere (maybe small and above text)
--- web/pages/session-create/session-create.svelte
+++ web/pages/session-create/session-create.svelte
@@ -2,4 +2,50 @@
 
 </script>
 
-hello
+<style lang="sass">
+    @use "./session-create.sass"
+</style>
+
+<main>
+    <div class="browser">
+        <div class="path">
+            /
+            <div class="path-item"><a href="">thing</a></div>
+            /
+            <div class="path-item"><a href="">other thing</a></div>
+        </div>
+        <div class="folders">
+            <div class="folder">
+                <a href="">..</a>
+            </div>
+            <div class="folder">
+                <input type="checkbox"/>
+                <a href="">a folder</a>
+            </div>
+            <div class="folder">
+                <input type="checkbox"/>
+                <a href="">Another Folder</a>
+            </div>
+        </div>
+    </div>
+    <div class="checkout">
+        <div class="items">
+            <div class="checkout-item">
+                <div class="name">
+                    an item
+                </div>
+                <div class="count">
+                    12
+                </div>
+            </div>
+            <div class="checkout-item">
+                <div class="name">
+                    another / item
+                </div>
+                <div class="count">
+                    120
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
